c9ce30a5c0bc0412721558c6b58d57022df31cbf
troymjose <troymjose@gmail.com>
2025-12-06 23:11:41 +0530

    styling

--- src/pyaiagent/openai/config.py
+++ src/pyaiagent/openai/config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Dict, Any, Type, Literal,TYPE_CHECKING
+from typing import Dict, Any, Type, Literal, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pydantic import BaseModel
